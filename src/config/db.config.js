f2c7f35bfef3d8820303e70c05770329d1d745dd
robbysuganda <robbyxsuganda@gmail.com>
2025-05-18 16:47:12 +0700

    chore: add config for deploy railway

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,12 +2,19 @@ const pg = require("pg");
 const { Pool } = pg;
 require("dotenv").config();
 
+//! Gunakan ini jika dilocal
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   idleTimeoutMillis: 1000,
+// });
+
+//! Gunakan ini jika production
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DB_URI,
   idleTimeoutMillis: 1000,
 });
 
